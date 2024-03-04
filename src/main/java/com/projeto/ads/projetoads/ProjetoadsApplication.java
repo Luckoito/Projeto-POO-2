f0ced747d36c8e82d76f4ab5c5259d8fa6f24e8e
Lucas Izidoro dos Santos <luckoito@icloud.com>
2024-03-04 09:17:19 -0300

    inittial commit

---
+++ src/main/java/com/projeto/ads/projetoads/ProjetoadsApplication.java
@@ -0,0 +1,14 @@
+package com.projeto.ads.projetoads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjetoadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoadsApplication.class, args);
+	}
+
+}
