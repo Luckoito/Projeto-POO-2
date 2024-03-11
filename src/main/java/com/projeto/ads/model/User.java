b620db47e058752b3f720af6c2b22f92747efdbc
Lucas Izidoro dos Santos <luckoito@icloud.com>
2024-03-11 09:36:39 -0300

    Aula 2

---
+++ src/main/java/com/projeto/ads/model/User.java
@@ -0,0 +1,102 @@
+package com.projeto.ads.model;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date birthDate;
+
+    private boolean active;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }   
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+
+
+}
--- src/main/java/com/projeto/ads/projetoads/ProjetoadsApplication.java
+++ src/main/java/com/projeto/ads/projetoads/ProjetoadsApplication.java
@@ -2,9 +2,20 @@ package com.projeto.ads.projetoads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+@EntityScan(basePackages = {"com.projeto.ads.model"})
+@ComponentScan(basePackages = {"com.projeto.ads"})
+@EnableJpaRepositories(basePackages = {"com.projeto.ads.repository"})
+@EnableTransactionManagement
+@EnableAutoConfiguration
 @SpringBootApplication
+
 public class ProjetoadsApplication {
 
 	public static void main(String[] args) {
